cb4cb17fc7b95ccbb2f67205993a690632b3327f
David B <59053870+David-Beale@users.noreply.github.com>
2021-06-03 13:09:32 +0100

    update toggle time icon

--- src/Components/Menu/Components/ToggleTime/ToggleTime.js
+++ src/Components/Menu/Components/ToggleTime/ToggleTime.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import Brightness6Icon from "@material-ui/icons/Brightness6";
+import { useDispatch, useSelector } from "react-redux";
+import Brightness5Icon from "@material-ui/icons/Brightness5";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 import { toggleTime } from "../../../../redux/settings";
 import { Tooltip } from "@material-ui/core";
@@ -15,6 +17,7 @@ const StyledTime = styled.div`
 `;
 
 export default function ToggleTime() {
+  const time = useSelector(({ settings }) => settings.time);
   const dispatch = useDispatch();
 
   const onClick = () => {
@@ -24,7 +27,13 @@ export default function ToggleTime() {
     <StyledTime>
       <Tooltip title="Toggle time of day">
         <StyledIconButtonTop onClick={onClick}>
-          <Brightness6Icon />
+          {
+            {
+              day: <Brightness5Icon />,
+              sunset: <Brightness4Icon />,
+              night: <NightsStayIcon />,
+            }[time]
+          }
         </StyledIconButtonTop>
       </Tooltip>
     </StyledTime>
