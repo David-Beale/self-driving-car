b48d7c91dd3dc44d153e53e9dddf99bf477bed71
David B <59053870+David-Beale@users.noreply.github.com>
2021-06-09 11:47:32 +0100

    scale obstacles

--- src/Components/Three/Obstacles/Obstalce.js
+++ src/Components/Three/Obstacles/Obstalce.js
@@ -4,14 +4,14 @@ import Cone from "./Cone";
 export default function Obstacle({ obstacle }) {
   const [ref] = useBox(() => ({
     mass: 1000,
-    position: [obstacle.x, 4, obstacle.z],
-    args: [2, 2, 2],
+    position: [obstacle.x, 2, obstacle.z],
+    args: [1, 1, 1],
     collisionFilterGroup: 1,
   }));
 
   return (
     <mesh ref={ref} frustumCulled={false}>
-      <Cone position = {[0,-1,0]}/>
+      <Cone position={[0, -0.5, 0]} scale={0.5} />
     </mesh>
   );
 }
